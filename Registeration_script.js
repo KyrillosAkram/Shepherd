7ce6e9c543c57d2d19c7ab225d1de3cdaf658c23
Kerillos Akram <kerillos.akram@gmail.com>
2022-10-14 22:46:09 +0200

    0001 add Registeration code
    bugs:
    Registeration_onLoad never been called after main div loading
    faceapi not working on input but canvas and visualised img

--- Registeration_script.js
+++ Registeration_script.js
@@ -1,4 +1,3 @@
-const imageUpload = document.getElementById('cam');
 //faceapi.tf.setBackend('cpu');
 console.log(faceapi.tf.getBackend());
 Promise.all([
@@ -8,31 +7,15 @@ Promise.all([
 ]).then(start)
 
 async function start() {
-  const container = document.createElement('div')
-  container.style.position = 'relative'
-  document.body.append(container)
-  const labeledFaceDescriptors = await loadLabeledImages()
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-  let image
-  let canvas
-  document.body.append('Loaded')
+  const imageUpload = document.getElementById('cam');
+  // document.body.append('Loaded')
+  M.toast({html: 'you can choose image'})
   imageUpload.addEventListener('change', async () => {
     if (image) image.remove()
     if (canvas) canvas.remove()
-    image = await faceapi.bufferToImage(imageUpload.files[0])
-    container.append(image)
-    canvas = faceapi.createCanvasFromMedia(image)
-    container.append(canvas)
-    const displaySize = { width: image.width, height: image.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-    results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-      drawBox.draw(canvas)
-    })
+  //the following encapsolate into on click event
+    // image = await faceapi.bufferToImage(imageUpload.files[0])
+    // const detections = await faceapi.detectSingleFaces(image).withFaceLandmarks().withFaceDescriptors()
   })
 }
 
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var debugging = true;
 var l = console.log
 var current_page = "Home"
 var pages = {
@@ -7,6 +8,26 @@ var pages = {
     Registed: { page_content: {} },
     New_session: { page_content: {} },
     Setting: { page_content: {} }
+}
+
+function App_onStartUp() {
+    // TODO : run loadup codevar db;
+    let request = window.indexedDB.open("sefain_brain", 1);
+    request.onupgradeneeded = (event) => {
+        db = event.target.result;
+        // const tx=db.transaction("notes","write");
+        let children = db.createObjectStore("children", { keyPath: "Name" });
+
+        debugging & console.log("upgrade called")
+    };
+    request.onsuccess = (event) => {
+        var db = event.target.result;
+        window.db = db;
+        debugging & console.log("successful");
+
+    };
+    request.onerror = (event) => { alert("errooooooooooooooooooooor") }
+
 }
 
 function swap_current_page_content(event) {
@@ -30,32 +51,73 @@ function calculate() {
 function fill_with_current_location() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(set_location);
-        M.toast({html:"Geolocation is detected" });
+        M.toast({ html: "Geolocation is detected" });
     } else {
-        M.toast({html:"Geolocation is not supported by this browser." });
+        M.toast({ html: "Geolocation is not supported by this browser." });
     }
 }
 
 
 
 function set_location(position) {
-    $("#Location").val( position.coords.latitude + ',' + position.coords.longitude)
+    $("#Location").val(position.coords.latitude + ',' + position.coords.longitude)
 }
 
 function x(e) {
     console.log(e)
 }
 
-function submit_registration(){
-    let check_counter=5
-    (Boolean($("#cam").val()))?check_counter--:M.toast({html:"please select/take image"});
-    (Boolean($("#Name").val()))?check_counter--:M.toast({html:"please enter the name"});
-    (Boolean($("#Address").val()))?check_counter--:M.toast({html:"please enter the address"});
-    (Boolean($("#Location").val()))?check_counter--:M.toast({html:"please enter the location"});
-    (Boolean($("#Class").val()))?check_counter--:M.toast({html:"please choose class"});
-    if(!check_counter){
-        
+function submit_registration() {
+    let check_counter = 5
+    (Boolean($("#cam").val())) ? check_counter-- : M.toast({ html: "please select/take image" });
+    (Boolean($("#Name").val())) ? check_counter-- : M.toast({ html: "please enter the name" });
+    (Boolean($("#Address").val())) ? check_counter-- : M.toast({ html: "please enter the address" });
+    (Boolean($("#Location").val())) ? check_counter-- : M.toast({ html: "please enter the location" });
+    (Boolean($("#Class").val())) ? check_counter-- : M.toast({ html: "please choose class" });
+    if (!check_counter) {// if all checks are ok
+        if(Boolean(window.db))
+        {
+            try {
+                let trx=window.db.transaction("children","readWrite")
+                let children=trx.objectStore("children");
+                children.add(
+                    {
+                        Name        :$("#Name").val(),
+                        Address     :$("#Address").val(),
+                        Location    :$("#Location").val(),
+                        Class       :$("#Class").val(),
+                        Discriptor  :new faceapi.LabeledFaceDescriptors($("#Name").val(), registration_discriptor),
+                    }
+                )
+                registration_discriptor=null;
+            } catch (error) {
+                debugging & console.log(error);
+                M.toast({html:error})
+            }
+        }
+    } else {
+        M.toast({html:"Please fill all filds !!!"})
     }
+}
+
+function Registeration_onLoad() {
+    debugging&console.log("Registeration_onLoad") 
+    Promise.all([
+        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+        faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+    ]).then(() => { // action event binder for registeration
+        const cam = document.getElementById('cam');
+        M.toast({ html: 'you can choose image' })
+        var registration_discriptor = null;
+        cam.addEventListener('change', async () => {
+            //TODO : need async improvment
+            debugging&console.log("cam change called")
+            registration_discriptor = await faceapi.detectSingleFace(cam).withFaceLandmarks().withFaceDescriptor().then(() => {
+                $("#registeration_submit").removeClass("disable");
+            })
+        })
+    })
 }
 
 // register ws //
