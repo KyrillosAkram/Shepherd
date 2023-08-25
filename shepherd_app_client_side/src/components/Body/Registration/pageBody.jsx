a5bbd847edb82460bfe92484215abc67ae7366fd
KyrillosAkram <kerillos.akram@gmail.com>
2023-08-25 16:25:52 +0300

    0038 enhancing registration #0001
    1 - add tensor flow clean up after submiting but this not a complet fix
    2 - add form clean up after submiting
    3 - resize the added image to fit screen

--- shepherd_app_client_side/src/components/Body/Registration/pageBody.jsx
+++ shepherd_app_client_side/src/components/Body/Registration/pageBody.jsx
@@ -33,64 +33,59 @@ function Registration_page_body() {
         }
     }
 
-    function submit_registration() {
-    let check_counter = 7
-    //TODO: implement warning on snak bar
-    !Boolean(document.getElementById("registration_cam").value =='') ? check_counter-- : console.log( "please select/take image" );
-    (Boolean(personName     )) ? check_counter-- : console.log( "please enter the name"      );
-    (Boolean(personAddress  )) ? check_counter-- : console.log( "please enter the address"   );
-    (Boolean(geoLocation    )) ? check_counter-- : console.log( "please enter the location"  );
-    (Boolean(personClass    )) ? check_counter-- : console.log( "please choose class"        );
-    (Boolean(personPhone    )) ? check_counter-- : console.log( "please enter the telephone" );
-    (Boolean(personBirthdate)) ? check_counter-- : console.log( "please enter the birthdate" );
-    if (!check_counter) {// if all checks are ok
-        if (Boolean(window.db)) {
-            try {
-                let trx = window.db.transaction("children", "readwrite")
-                let children = trx.objectStore("children");
-                Promise.all([new window.faceapi.LabeledFaceDescriptors(document.querySelector("#Name").value, [window.registration_discriptor.descriptor])]).then((values) => {
-                    children.put(
-                        {
-                            Name: personName,
-                            Address: personAddress,
-                            Location: geoLocation,
-                            Class: personClass,
-                            Discriptor: values[0],
-                            Telephone: personPhone,
-                            Birthdate: personBirthdate
-                        }
-                    )
-                    /*debugging &*/ console.log("children.add");
-                    /*debugging &*/ console.log(values)
-                    window.registration_discriptor = null
-                    setPersonName(undefined)
-                    setPersonAddress(undefined)
-                    setGeoLocation(undefined)
-                    setPersonClass(undefined   )
-                    setPersonPhone(undefined)
-                    setPersonBirthdate(undefined)
-                    // document.querySelector("#Name").value = ''
-                    // document.querySelector("#Address").value = ''
-                    // document.querySelector("#Location").value = ''
-                    // document.querySelector("#Class").value = '?'
-                    // document.querySelector("li.selected").classList.remove("selected")
-                    // document.querySelector("li.disabled").classList.add("selected")
-                    // document.querySelector("#telephone").value = ''
-                    // document.querySelector("#birthdate").value = ''
-                    // document.querySelector("#registeration_submit").classList.add("disabled")
-                    setActivation(true)
-                })
-                window.registration_discriptor = null;
-            } catch (error) {
+    function submit_registration()
+    {
+        let check_counter = 7
+        //TODO: implement warning on snak bar
+        !Boolean(document.getElementById("registration_cam").value == '') ? check_counter-- : console.log("please select/take image");
+        (Boolean(personName)) ? check_counter-- : console.log("please enter the name");
+        (Boolean(personAddress)) ? check_counter-- : console.log("please enter the address");
+        (Boolean(geoLocation)) ? check_counter-- : console.log("please enter the location");
+        (Boolean(personClass)) ? check_counter-- : console.log("please choose class");
+        (Boolean(personPhone)) ? check_counter-- : console.log("please enter the telephone");
+        (Boolean(personBirthdate)) ? check_counter-- : console.log("please enter the birthdate");
+        if (!check_counter) {// if all checks are ok
+            if (Boolean(window.db)) {
+                try {
+                    let trx = window.db.transaction("children", "readwrite")
+                    let children = trx.objectStore("children");
+                    Promise.all([new window.faceapi.LabeledFaceDescriptors(document.querySelector("#Name").value, [window.registration_discriptor.descriptor])]).then((values) => {
+                        children.put(
+                            {
+                                Name: personName,
+                                Address: personAddress,
+                                Location: geoLocation,
+                                Class: personClass,
+                                Discriptor: values[0],
+                                Telephone: personPhone,
+                                Birthdate: personBirthdate
+                            }
+                        )
+                        /*debugging &*/ console.log("children.add");
+                        /*debugging &*/ console.log(values)
+                    }).finally(() => {
+                        window.registration_discriptor = null
+                        setPersonName('')
+                        setPersonAddress('')
+                        setGeoLocation('')
+                        setPersonClass('')
+                        setPersonPhone('')
+                        setPersonBirthdate('')
+                        setActivation(true)
+                        document.getElementById("image_section").replaceChildren()
+
+                    })
+                    window.registration_discriptor = null;
+                } catch (error) {
                 /*debugging &*/ console.log(error);
-                console.log( error )
+                    console.log(error)
+                }
             }
+        } else {
+            console.log("Please fill all filds !!!")
         }
-    } else {
-        console.log( "Please fill all filds !!!" )
+
     }
-    
-}
     
     return (
         <Container component="main" maxWidth="xs">
@@ -108,22 +103,36 @@ function Registration_page_body() {
                          <Grid item xs={12} >
                          <input id="registration_cam" type="file" accept="image/*;capture=camera" hidden={true} onChange={
                              async () => {
-                                //TODO : need async improvment
                                 /*debugging &*/ console.log("cam change called")
-                                let image, canvas;
+                                let image;//, canvas;
                                 const container = document.getElementById("image_section")
                                 if (image) image.remove()
-                                if (canvas) canvas.remove()
+                                // if (canvas) canvas.remove()
                                 image = await window.faceapi.bufferToImage(document.getElementById("registration_cam").files[0])
+                                image.style.height = 'auto'
+                                image.style.width = `${document.querySelector('input#Name').clientWidth}px`
                     
                                 container.append(image)
-                                window.faceapi.detectSingleFace(image).withFaceLandmarks().withFaceDescriptor().then((event) => {
-                                    console.log(event)
-                                    window.registration_discriptor = { ...event }
-                                    /*debugging &*/ console.log("remove disable");
-                                    setActivation(false)
-                                    // document.querySelector("#registeration_submit").removeClass("disabled")
-                                }).catch((e)=>console.log(e))
+                                //kakram:the following line needed to make the start of the allocated memory for this operation to be freed after finishing
+                                //please check the following issue for more details https://github.com/vladmandic/face-api/issues/25
+                                window.faceapi.tf.engine().startScope();
+                                await window.faceapi.detectSingleFace(image).withFaceLandmarks().withFaceDescriptor().then(
+                                    (event) =>
+                                    {
+                                        console.log(event)
+                                        window.registration_discriptor = { ...event }
+                                        /*debugging &*/ console.log("remove disable");
+                                        setActivation(false)
+                                    }
+                                ).catch(
+                                    (e)=>console.log(e)
+                                ).finally(
+                                    () =>
+                                    {
+                                        window.faceapi.tf.engine().endScope();//kakram: to deallocate the memory selected in last scope
+                                        console.log('Experiment completed');
+                                    }
+                                  );
                             }
                          }/>
                          <Fab color="primary" aria-label="add" size="medium"
